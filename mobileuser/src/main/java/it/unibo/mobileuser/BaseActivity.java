334164a267e72e3775bcc895ca01d47ba1c25a0c
agleoni <annagiulia.leoni@studio.unibo.it>
2017-08-21 17:06:12 +0200

    Add javadoc to BaseActivity

--- mobileuser/src/main/java/it/unibo/mobileuser/BaseActivity.java
+++ mobileuser/src/main/java/it/unibo/mobileuser/BaseActivity.java
@@ -4,16 +4,34 @@ import android.app.Fragment;
 import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 
+/**
+ * Base activity from which every activity of the App extends
+ */
 public class BaseActivity extends AppCompatActivity {
 
-    protected void setFragment(Fragment fragment, int containerId, boolean addBack) {
+    /**
+     * Sets the fragment on the view indicated by containerId.
+     *
+     * @param fragment    fragment to set
+     * @param containerId layout resource in which to put the fragment
+     * @param addBack     if true add the transaction to the back stack
+     */
+    protected void setFragment(final Fragment fragment, final int containerId, final boolean addBack) {
         setFragment(fragment, containerId, fragment.getClass().getName(), addBack);
     }
 
-    private void setFragment(Fragment fragment, int containerId, String tag, boolean addBack) {
+    /**
+     * Sets the fragment on the view indicated by containerId.
+     *
+     * @param fragment    fragment to set
+     * @param containerId layout resource in which to put the fragment
+     * @param tag         tag to identify the fragment on the activity
+     * @param addBack     if true add the transaction to the back stack
+     */
+    private void setFragment(final Fragment fragment, final int containerId, final String tag, final boolean addBack) {
         if (isFinishing())
             return;
-        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+        final FragmentTransaction transaction = getFragmentManager().beginTransaction();
         transaction.replace(containerId, fragment, tag);
         if (addBack)
             transaction.addToBackStack(tag);
