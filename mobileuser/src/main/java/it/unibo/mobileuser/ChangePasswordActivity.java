12e58982724061ab0965856f7eebc83dde00d686
Elisa Casadio <elisa.casadio7@studio.unibo.it>
2017-08-21 12:56:24 +0200

    Add new activity to change password of account

---
+++ mobileuser/src/main/java/it/unibo/mobileuser/ChangePasswordActivity.java
@@ -0,0 +1,26 @@
+package it.unibo.mobileuser;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * A class that allows to show the graphical interface to change the password of account and save the changes on server.
+ */
+public class ChangePasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_password);
+        
+        final Button saveButton = (Button) findViewById(R.id.save_button);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View view) {
+                //TODO: Add method to send to server the changes
+            }
+        });
+    }
+}
--- mobileuser/src/main/java/it/unibo/mobileuser/ProfileActivity.java
+++ mobileuser/src/main/java/it/unibo/mobileuser/ProfileActivity.java
@@ -1,7 +1,10 @@
 package it.unibo.mobileuser;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * A class that allows to show user profile and manage interaction with the graphical interface.
@@ -12,5 +15,13 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        final Button changePassword = (Button) findViewById(R.id.password_change_button);
+        changePassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View view) {
+                startActivity(new Intent(view.getContext(), ChangePasswordActivity.class));
+            }
+        });
     }
 }
