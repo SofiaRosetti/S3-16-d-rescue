a725632037ea31530e2f4b2f7f5942b6ef61772f
agleoni <annagiulia.leoni@studio.unibo.it>
2017-08-21 17:53:39 +0200

    Replace ActionBar with Toolbar

--- mobileuser/src/main/java/it/unibo/mobileuser/SplashActivity.java
+++ mobileuser/src/main/java/it/unibo/mobileuser/SplashActivity.java
@@ -1,13 +1,19 @@
 package it.unibo.mobileuser;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
-public class SplashActivity extends AppCompatActivity {
+public class SplashActivity extends BaseActivity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        this.toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(this.toolbar);
+
     }
 }
