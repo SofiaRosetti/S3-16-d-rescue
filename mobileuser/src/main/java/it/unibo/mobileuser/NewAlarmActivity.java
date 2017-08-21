61a1807eee0573f1e7c4ded36027e022a8e6131a
Elisa Casadio <elisa.casadio7@studio.unibo.it>
2017-08-21 10:27:02 +0200

    Add new activity to report new alert

---
+++ mobileuser/src/main/java/it/unibo/mobileuser/NewAlarmActivity.java
@@ -0,0 +1,12 @@
+package it.unibo.mobileuser;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class NewAlarmActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
