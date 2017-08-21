22300f436ce5f19acd75f28e9424967b09850e78
Elisa Casadio <elisa.casadio7@studio.unibo.it>
2017-08-21 12:32:54 +0200

    Add javadoc

--- mobileuser/src/main/java/it/unibo/mobileuser/NewAlarmActivity.java
+++ mobileuser/src/main/java/it/unibo/mobileuser/NewAlarmActivity.java
@@ -2,20 +2,36 @@ package it.unibo.mobileuser;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
+/**
+ * A class that allows to show graphical interface to report new alarm and sends the alarm to server.
+ */
 public class NewAlarmActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_alarm);
 
         //TODO: Change with an array of events coming from database
-        Spinner spinner = (Spinner) findViewById(R.id.event_type_spinner);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.event_type_array, android.R.layout.simple_spinner_item);
+        final Spinner spinner = (Spinner) findViewById(R.id.event_type_spinner);
+        final ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this,
+                R.array.event_type_array,
+                android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
+
+        final Button sendButton = (Button) findViewById(R.id.send_button);
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View view) {
+                //TODO: Add method to send new alarm with event type, description, latitude and longitude to server.
+            }
+        });
     }
 }
--- mobileuser/src/main/java/it/unibo/mobileuser/ProfileActivity.java
+++ mobileuser/src/main/java/it/unibo/mobileuser/ProfileActivity.java
@@ -3,10 +3,13 @@ package it.unibo.mobileuser;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+/**
+ * A class that allows to show user profile and manage interaction with the graphical interface.
+ */
 public class ProfileActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
     }
