7508035a63d312a4ef5e477b1fe6a71b89ed0c2f
Elisa Casadio <elisa.casadio7@studio.unibo.it>
2017-08-21 10:29:41 +0200

    Add new layout to report new alarm

--- mobileuser/src/main/java/it/unibo/mobileuser/NewAlarmActivity.java
+++ mobileuser/src/main/java/it/unibo/mobileuser/NewAlarmActivity.java
@@ -8,5 +8,6 @@ public class NewAlarmActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_alarm);
     }
 }
