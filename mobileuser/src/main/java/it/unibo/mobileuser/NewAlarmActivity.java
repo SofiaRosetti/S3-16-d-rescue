f6b462e090be3c6835d0ff82274e2f7c3f3ff612
Elisa Casadio <elisa.casadio7@studio.unibo.it>
2017-08-21 10:51:32 +0200

    Implement layout to report new alarm

--- mobileuser/src/main/java/it/unibo/mobileuser/NewAlarmActivity.java
+++ mobileuser/src/main/java/it/unibo/mobileuser/NewAlarmActivity.java
@@ -2,6 +2,8 @@ package it.unibo.mobileuser;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class NewAlarmActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class NewAlarmActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_alarm);
+
+        //TODO: Change with an array of events coming from database
+        Spinner spinner = (Spinner) findViewById(R.id.event_type_spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.event_type_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
     }
 }
