f8e75a1474e3318eb58eea1aa98503968bd01ce9
agleoni <annagiulia.leoni@studio.unibo.it>
2017-08-23 20:59:39 +0200

    Add javadoc to StringUtils

--- utils-java/src/main/java/it/unibo/drescue/StringUtils.java
+++ utils-java/src/main/java/it/unibo/drescue/StringUtils.java
@@ -3,14 +3,29 @@ package it.unibo.drescue;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * Utiliry class for strings.
+ */
 public class StringUtils {
 
     private static final String EMAIL_REGEX = "^[-\\w\\.-]+@([\\w\\-]+\\.)+[A-Z]{2,}$";
 
+    /**
+     * Checks if a string is null or empty
+     *
+     * @param string
+     * @return true if is null or empty, false otherwise
+     */
     public static boolean isAValidString(final String string) {
         return string != null && !string.trim().isEmpty();
     }
 
+    /**
+     * Checks if a string is in a valid email format
+     *
+     * @param email
+     * @return true if matches a regex, false otherwise
+     */
     public static boolean isAValidEmail(final String email) {
         final CharSequence charSequenceEmail = email;
 
