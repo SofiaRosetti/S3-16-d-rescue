ac8a678345d745dfe23216e03e6c2ae648a75658
agleoni <annagiulia.leoni@studio.unibo.it>
2017-08-23 22:57:14 +0200

    Add RequestDelegate and ServerResponse to handle server response

---
+++ mobileuser/src/main/java/it/unibo/mobileuser/connection/ServerResponse.java
@@ -0,0 +1,14 @@
+package it.unibo.mobileuser.connection;
+
+/**
+ * Class that handle the server response.
+ */
+public class ServerResponse implements RequestDelegate {
+
+    @Override
+    public void onReceivingResponse(final String response) {
+
+    }
+
+
+}
--- mobileuser/src/main/java/it/unibo/mobileuser/utils/ServerUtils.java
+++ mobileuser/src/main/java/it/unibo/mobileuser/utils/ServerUtils.java
@@ -1,8 +1,46 @@
 package it.unibo.mobileuser.utils;
 
+import it.unibo.mobileuser.connection.RequestItemImpl;
+
 public class ServerUtils {
 
     public static final String SERVER_ADDRESS = "ec2-13-58-168-22.us-east-2.compute.amazonaws.com";
     public static final int PORT = 4321;
 
+    /**
+     * To handle server response
+     */
+    public static final int RESPONSE_SUCCESS_CODE = 200;
+    public static final int RESPONSE_ERROR_CODE = 400;
+    public static final String CODE = "code";
+    public static final String DATA = "data";
+
+    /**
+     * Json keys
+     */
+    private static final String EMAIL = "email";
+    private static final String PASSWORD = "password";
+    private static final String NAME = "name";
+    private static final String SURNAME = "surname";
+    private static final String PHONE_NUMBER = "phoneNumber";
+
+    /**
+     * Requests
+     */
+    public static RequestItemImpl signIn(final String email, final String password, final String name, final String surname, final String phone) {
+        final RequestItemImpl signInRequest = new RequestItemImpl();
+        signInRequest.putKeyValuePair(EMAIL, email);
+        signInRequest.putKeyValuePair(PASSWORD, password);
+        signInRequest.putKeyValuePair(NAME, name);
+        signInRequest.putKeyValuePair(SURNAME, surname);
+        signInRequest.putKeyValuePair(PHONE_NUMBER, phone);
+        return signInRequest;
+    }
+
+    public static RequestItemImpl login(final String email, final String password) {
+        final RequestItemImpl signInRequest = new RequestItemImpl();
+        signInRequest.putKeyValuePair(EMAIL, email);
+        signInRequest.putKeyValuePair(PASSWORD, password);
+        return signInRequest;
+    }
 }
