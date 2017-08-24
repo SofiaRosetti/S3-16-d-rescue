a528cbb0097e98e400e4a390fc793cc1b3655dd4
Sofia Rosetti <sofia.rosetti@studio.unibo.it>
2017-08-24 21:31:41 +0200

    Create user data model interface

---
+++ utils-java/src/main/java/it/unibo/drescue/model/User.java
@@ -0,0 +1,37 @@
+package it.unibo.drescue.model;
+
+/**
+ * An interface modelling a User.
+ */
+public interface User {
+
+    /**
+     * @return the user ID
+     */
+    int getID();
+
+    /**
+     * @return the user name
+     */
+    String getName();
+
+    /**
+     * @return the user surname
+     */
+    String getSurname();
+
+    /**
+     * @return the user email
+     */
+    String getEmail();
+
+    /**
+     * @return the user password
+     */
+    String getPassword();
+
+    /**
+     * @return the user phone number
+     */
+    String getPhoneNumber();
+}
