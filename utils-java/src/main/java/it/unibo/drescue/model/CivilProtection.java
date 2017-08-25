74a4dcac753d84688a64182c3cd606ea41d8297a
Sofia Rosetti <sofia.rosetti@studio.unibo.it>
2017-08-25 22:07:26 +0200

    Create civil protection data model interface

---
+++ utils-java/src/main/java/it/unibo/drescue/model/CivilProtection.java
@@ -0,0 +1,19 @@
+package it.unibo.drescue.model;
+
+/**
+ * An interface modelling a Civil Protection.
+ */
+
+public interface CivilProtection {
+
+    /**
+     * @return the civil protection ID
+     */
+    String getCpID();
+
+    /**
+     * @return the civil protection password
+     */
+    String getPassword();
+
+}
