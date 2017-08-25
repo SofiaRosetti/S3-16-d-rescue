4a2291fada75734ad20e44901236e9b4aac0bc5d
agleoni <annagiulia.leoni@studio.unibo.it>
2017-08-25 21:58:30 +0200

    Refactor of BaseActivity, add showDialog

--- mobileuser/src/main/java/it/unibo/mobileuser/BaseActivity.java
+++ mobileuser/src/main/java/it/unibo/mobileuser/BaseActivity.java
@@ -1,7 +1,6 @@
 package it.unibo.mobileuser;
 
-import android.app.Fragment;
-import android.app.FragmentTransaction;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 
 /**
@@ -10,32 +9,16 @@ import android.support.v7.app.AppCompatActivity;
 public class BaseActivity extends AppCompatActivity {
 
     /**
-     * Sets the fragment on the view indicated by containerId.
+     * Shows a simple dialog with a title and a message
      *
-     * @param fragment    fragment to set
-     * @param containerId layout resource in which to put the fragment
-     * @param addBack     if true add the transaction to the back stack
+     * @param title
+     * @param message
      */
-    protected void setFragment(final Fragment fragment, final int containerId, final boolean addBack) {
-        setFragment(fragment, containerId, fragment.getClass().getName(), addBack);
+    protected final void showDialog(final int title, final int message) {
+        new AlertDialog.Builder(this)
+                .setTitle(title)
+                .setMessage(message)
+                .setPositiveButton(R.string.ok, null)
+                .show();
     }
-
-    /**
-     * Sets the fragment on the view indicated by containerId.
-     *
-     * @param fragment    fragment to set
-     * @param containerId layout resource in which to put the fragment
-     * @param tag         tag to identify the fragment on the activity
-     * @param addBack     if true add the transaction to the back stack
-     */
-    private void setFragment(final Fragment fragment, final int containerId, final String tag, final boolean addBack) {
-        if (isFinishing())
-            return;
-        final FragmentTransaction transaction = getFragmentManager().beginTransaction();
-        transaction.replace(containerId, fragment, tag);
-        if (addBack)
-            transaction.addToBackStack(tag);
-        transaction.commitAllowingStateLoss();
-    }
-
 }
