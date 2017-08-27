bf99c1a2cc0d42c059fe6757ec7aebb7d1dd5cb9
Samantha Bandini <samantha.bandini3@studio.unibo.it>
2017-08-27 16:28:43 +0200

    Separates DB connection from data access logic. Now DBConnection contains only methods to establish the connection (tested in DBConnectionImplTest)

--- server/src/main/java/it/unibo/drescue/database/DBConnection.java
+++ server/src/main/java/it/unibo/drescue/database/DBConnection.java
@@ -17,40 +17,10 @@ public interface DBConnection {
     void closeConnection();
 
     /**
-     * @param email
-     * @return the userID if the user with the given email exist,
-     * -1 otherwise
-     */
-    int getUserId(String email);
-
-    /**
-     * This method register a given user into DB
+     * Check if connection is still valid
      *
-     * @param email
-     * @param password
-     * @param name
-     * @param surname
-     * @param phoneNumber
-     * @return true if the given user is added into DB
-     * false if the given user email already exist in the DB
+     * @return true if the connection responds within 5 seconds, false otherwise
      */
-    boolean registerUser(String email, String password,
-                         String name, String surname, String phoneNumber);
-
-    /**
-     * Delete a user from DB with the given userID (taken with getUser(email) method)
-     *
-     * @param userID
-     * @return false if something goes wrong
-     */
-    boolean unregisterUser(int userID);
-
-    /**
-     * @param email
-     * @param password
-     * @return true if the credentials are valid, false otherwise
-     */
-    boolean login(String email, String password);
-
+    boolean isValid();
 
 }
