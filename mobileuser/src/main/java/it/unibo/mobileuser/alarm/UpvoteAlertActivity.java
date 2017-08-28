c8e5a960d3e4c9e0226955165c2977f70f34ed4e
agleoni <annagiulia.leoni@studio.unibo.it>
2017-08-28 13:23:26 +0200

    Add method to upvote alerts

--- mobileuser/src/main/java/it/unibo/mobileuser/alarm/UpvoteAlertActivity.java
+++ mobileuser/src/main/java/it/unibo/mobileuser/alarm/UpvoteAlertActivity.java
@@ -3,10 +3,16 @@ package it.unibo.mobileuser.alarm;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.widget.ListView;
+import android.widget.Toast;
+import com.google.gson.JsonObject;
 import it.unibo.drescue.model.Alert;
 import it.unibo.drescue.model.AlertImplBuilder;
 import it.unibo.mobileuser.R;
+import it.unibo.mobileuser.connection.AbstractServerResponse;
+import it.unibo.mobileuser.connection.RequestAsyncTask;
 import it.unibo.mobileuser.gps.GpsActivityImpl;
+import it.unibo.mobileuser.utils.ServerUtils;
+import it.unibo.mobileuser.utils.Utils;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -43,7 +49,8 @@ public class UpvoteAlertActivity extends GpsActivityImpl {
                     .setTitle(R.string.upvote)
                     .setMessage(R.string.upvote_message)
                     .setPositiveButton(R.string.ok, (dialogInterface, i) -> {
-                        //TODO upvote alert
+                        final Alert alert = this.alertList.get(position);
+                        upvoteAlert(alert.getAlertID());
                         dialogInterface.dismiss();
                     })
                     .setNeutralButton(R.string.alert_negative_button, (dialogInterface, i) -> {
@@ -54,6 +61,31 @@ public class UpvoteAlertActivity extends GpsActivityImpl {
 
         //TODO requestAlerts
 
+    }
+
+    /**
+     * Performs the upvote by the logged user of the alert identified by alertID.
+     *
+     * @param alertID
+     */
+    private void upvoteAlert(final int alertID) {
+        final String userID = Utils.getUserIDfromSharedPreferences(getApplicationContext());
+
+        System.out.println("[UpvoteAlertActivity] upvoteAlert: userID=" + userID + " alertID=" + alertID);
+
+        new RequestAsyncTask(ServerUtils.upvoteAlert(userID, String.valueOf(alertID)),
+                new AbstractServerResponse<JsonObject>() {
+                    @Override
+                    public void onSuccessfulRequest(final JsonObject data) {
+                        Toast.makeText(UpvoteAlertActivity.this, R.string.upvote_successful, Toast.LENGTH_LONG).show();
+                        //TODO requestAlerts
+                    }
+
+                    @Override
+                    public void onErrorRequest(final int code) {
+                        showDialog(R.string.upvote, R.string.incorrect_upvote);
+                    }
+                });
     }
 
 }
