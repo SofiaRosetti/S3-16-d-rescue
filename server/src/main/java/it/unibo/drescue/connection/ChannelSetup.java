7f3858288aed4c917eaa4c847a474beea993a3e4
agleoni <annagiulia.leoni@studio.unibo.it>
2017-09-01 15:52:07 +0200

    Add server side classes to set up channels and receive messages

---
+++ server/src/main/java/it/unibo/drescue/connection/ChannelSetup.java
@@ -0,0 +1,19 @@
+package it.unibo.drescue.connection;
+
+/**
+ * Class that handles the creation of every channel on which the
+ * server can receive requests.
+ */
+public class ChannelSetup {
+
+    /**
+     * Creates channel on the given connection.
+     *
+     * @param connection
+     */
+    public ChannelSetup(final RabbitMQConnection connection) {
+
+        //TODO setup channels
+
+    }
+}
