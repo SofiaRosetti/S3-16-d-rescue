be26ae402aaa879ddad85f9ada6a2d30a83eb95a
Samantha Bandini <samantha.bandini3@studio.unibo.it>
2017-09-01 18:17:09 +0200

    Modify method for getting a java list from a file that contains a jsonArray, now it works with generics

--- server/src/main/java/it/unibo/drescue/utils/JsonFileUtils.java
+++ server/src/main/java/it/unibo/drescue/utils/JsonFileUtils.java
@@ -1,39 +1,38 @@
 package it.unibo.drescue.utils;
 
 import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
-import it.unibo.drescue.model.DistrictImpl;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.lang.reflect.Type;
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class JsonFileUtils {
 
-    public final static String JSON_DISTRICTS_PATH = "server/res/districts.json";
     private final Gson gson = new Gson();
 
-    public JsonFileUtils() {
-    }
-
     /**
-     * Get an ArrayList of District from a json file
+     * Get a generic list of objects from a json file
+     * <p>
+     * Note: the json file must be in the format of JsonArray:
+     * ex. : [{jsonObj1},{jsonObj2},{jsonObj3},...]
+     * where the JsonObjects keys must be the same as parameters in the object's model of the specified type
+     * </p>
      *
      * @param path indicates the path to the json file
-     * @return an ArrayList of Districts with all districts contained in the json file
+     * @param type indicates the type of the class of the object to get
+     * @param <T>  the type of elements maintained by this list
+     * @return a List of Objects with all objects contained in the json file
      */
-    public ArrayList<DistrictImpl> getDistrictsFromFile(final String path) {
-        ArrayList<DistrictImpl> districts = null;
+    public <T> List<T> getListFromJsonFile(final String path, final Class<T[]> type) {
+        T[] array = null;
         try {
             final BufferedReader bufferedReader = new BufferedReader(new FileReader(path));
-            final Type type = new TypeToken<ArrayList<DistrictImpl>>() {
-            }.getType();
-            districts = this.gson.fromJson(bufferedReader, type);
+            array = this.gson.fromJson(bufferedReader, type);
         } catch (final FileNotFoundException e) {
             e.printStackTrace();
         }
-        return districts;
+        return Arrays.asList(array);
     }
 }
