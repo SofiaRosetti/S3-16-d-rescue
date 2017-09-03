477b4c5b51491fd2b89671ba922d7e1afc553a3a
Sofia Rosetti <sofia.rosetti@studio.unibo.it>
2017-09-03 14:34:00 +0200

    Add username and password labels to CP login view

--- civilprotection/src/main/scala/it/unibo/drescue/view/Login.scala
+++ civilprotection/src/main/scala/it/unibo/drescue/view/Login.scala
@@ -4,17 +4,34 @@ import scalafx.application.JFXApp
 import scalafx.application.JFXApp.PrimaryStage
 import scalafx.geometry.Insets
 import scalafx.scene.Scene
+import scalafx.scene.control.Label
 import scalafx.scene.layout.GridPane
+import scalafx.scene.text.Font
 
 object Login extends JFXApp {
 
   stage = new PrimaryStage {
     title = "D-rescue"
     scene = new Scene {
-      val grid = new GridPane() {
+      val grid = new GridPane {
         hgap = 10
         vgap = 10
         padding = Insets(100)
+
+        val defaultFont = new Font(20)
+
+        val usernameLabel = new Label {
+          text = "Username:"
+          font = defaultFont
+        }
+        add(usernameLabel, 0, 0)
+
+        val passwordLabel = new Label {
+          text = "Password:"
+          font = defaultFont
+        }
+        add(passwordLabel, 0, 1)
+
       }
       content = grid
     }
