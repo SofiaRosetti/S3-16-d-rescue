5da26dd3af04fbfd9d2dc5f8b3a1c080efcf4f89
Sofia Rosetti <sofia.rosetti@studio.unibo.it>
2017-09-04 13:16:59 +0200

    Add last alarms title table and relative hbox

--- civilprotection/src/main/scala/it/unibo/drescue/view/HomeView.scala
+++ civilprotection/src/main/scala/it/unibo/drescue/view/HomeView.scala
@@ -2,9 +2,10 @@ package it.unibo.drescue.view
 
 import scalafx.application.JFXApp
 import scalafx.application.JFXApp.PrimaryStage
-import scalafx.geometry.Insets
+import scalafx.geometry.{Insets, Pos}
 import scalafx.scene.Scene
-import scalafx.scene.layout.GridPane
+import scalafx.scene.control.Label
+import scalafx.scene.layout.{GridPane, HBox}
 import scalafx.scene.text.Font
 
 object HomeView extends JFXApp {
@@ -22,6 +23,20 @@ object HomeView extends JFXApp {
         val titleFont = new Font(30)
         val checkBoxFont = new Font(18)
         val buttonFont = new Font(25)
+
+        val lastAlarmsLabel = new Label {
+          text = "Last alarms:"
+          font = titleFont
+          padding = Insets(20)
+        }
+        val titleBox = new HBox {
+          children = lastAlarmsLabel
+          alignment = Pos.Center
+        }
+        add(titleBox, 0, 0)
+        GridPane.setConstraints(titleBox, 0, 0, 2, 1)
+
+
       }
       content = grid
     }
