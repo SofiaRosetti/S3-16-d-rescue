ee7bc33d7367337f564db3e68b9d372dcf9e1e9f
Sofia Rosetti <sofia.rosetti@studio.unibo.it>
2017-09-04 13:45:51 +0200

    Add title label and relative hbox to CP rescue view

--- civilprotection/src/main/scala/it/unibo/drescue/view/RescueView.scala
+++ civilprotection/src/main/scala/it/unibo/drescue/view/RescueView.scala
@@ -2,9 +2,10 @@ package it.unibo.drescue.view
 
 import scalafx.application.JFXApp
 import scalafx.application.JFXApp.PrimaryStage
-import scalafx.geometry.Insets
+import scalafx.geometry.{Insets, Pos}
 import scalafx.scene.Scene
-import scalafx.scene.layout.GridPane
+import scalafx.scene.control.Label
+import scalafx.scene.layout.{GridPane, HBox}
 import scalafx.scene.text.Font
 
 object RescueView extends JFXApp {
@@ -12,13 +13,25 @@ object RescueView extends JFXApp {
   stage = new PrimaryStage {
     title = "D-rescue"
     scene = new Scene {
-      val grid = new GridPane() {
+      val grid = new GridPane {
         hgap = 10
         vgap = 10
         padding = Insets(100)
 
         val defaultFont = new Font(25)
         val titleFont = new Font(30)
+
+        val titleLabel = new Label {
+          text = "New rescue:"
+          font = titleFont
+          padding = Insets(20)
+        }
+        val titleBox = new HBox {
+          children = titleLabel
+          alignment = Pos.Center
+        }
+        add(titleBox, 0, 0)
+        GridPane.setConstraints(titleBox, 0, 0, 3, 1)
 
       }
       content = grid
