3f559a5343b3aefc79c232a8fdd9756c22dc9f86
Sofia Rosetti <sofia.rosetti@studio.unibo.it>
2017-09-04 13:58:17 +0200

    Add select and insert team labels and relative hbox

--- civilprotection/src/main/scala/it/unibo/drescue/view/TeamView.scala
+++ civilprotection/src/main/scala/it/unibo/drescue/view/TeamView.scala
@@ -33,6 +33,28 @@ object TeamView extends JFXApp {
         add(titleBox, 0, 0)
         GridPane.setConstraints(titleBox, 0, 0, 2, 1)
 
+        val selectTeamLabel = new Label {
+          text = "Select existing team:"
+          font = defaultFont
+          padding = Insets(20)
+        }
+        val selectBox = new HBox {
+          children = selectTeamLabel
+          alignment = Pos.Center
+        }
+        add(selectBox, 0, 1)
+
+        val insertTeamLabel = new Label {
+          text = "Insert new team:"
+          font = defaultFont
+          padding = Insets(20)
+        }
+        val insertBox = new HBox {
+          children = insertTeamLabel
+          alignment = Pos.Center
+        }
+        add(insertBox, 1, 1)
+
       }
       content = grid
     }
