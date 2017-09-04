fa4937726ef2818b18ce918aa7f29740adc46bcb
Sofia Rosetti <sofia.rosetti@studio.unibo.it>
2017-09-04 13:14:53 +0200

    Add fonts to CP home view

--- civilprotection/src/main/scala/it/unibo/drescue/view/HomeView.scala
+++ civilprotection/src/main/scala/it/unibo/drescue/view/HomeView.scala
@@ -5,17 +5,23 @@ import scalafx.application.JFXApp.PrimaryStage
 import scalafx.geometry.Insets
 import scalafx.scene.Scene
 import scalafx.scene.layout.GridPane
+import scalafx.scene.text.Font
 
 object HomeView extends JFXApp {
 
   stage = new PrimaryStage {
     title = "D-rescue"
     scene = new Scene {
-      val grid = new GridPane() {
+      val grid = new GridPane {
         resizable = false
         hgap = 10
         vgap = 10
         padding = Insets(50)
+
+        val defaultFont = new Font(20)
+        val titleFont = new Font(30)
+        val checkBoxFont = new Font(18)
+        val buttonFont = new Font(25)
       }
       content = grid
     }
