ae1d2314bfc511bff3bf6e2f0d05fd5e2e32ea90
Elisa Casadio <elisa.casadio7@studio.unibo.it>
2017-09-05 15:35:59 +0200

    Add method to create a simple dialog

--- mobileuser/src/main/java/it/unibo/mobileuser/BaseActivity.java
+++ mobileuser/src/main/java/it/unibo/mobileuser/BaseActivity.java
@@ -9,16 +9,26 @@ import android.support.v7.app.AppCompatActivity;
 public class BaseActivity extends AppCompatActivity {
 
     /**
-     * Shows a simple dialog with a title and a message
+     * Shows a simple dialog with a title and a message.
      *
-     * @param title
-     * @param message
+     * @param title   title of dialog
+     * @param message message of dialog
      */
-    protected final void showDialog(final int title, final int message) {
-        new AlertDialog.Builder(this)
+    protected void showDialog(final int title, final String message) {
+        new AlertDialog.Builder(this, R.style.alert_dialog_theme)
                 .setTitle(title)
                 .setMessage(message)
                 .setPositiveButton(R.string.ok, null)
                 .show();
+    }
+
+    /**
+     * Shows a simple dialog with a title and a message.
+     *
+     * @param title   title of dialog
+     * @param message message of dialog
+     */
+    protected final void showDialog(final int title, final int message) {
+        showDialog(title, getResources().getString(message));
     }
 }
