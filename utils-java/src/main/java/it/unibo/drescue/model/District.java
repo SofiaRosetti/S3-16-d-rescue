aa589a3814e983024ea7a454e847cf88e6112aed
Sofia Rosetti <sofia.rosetti@studio.unibo.it>
2017-09-07 14:16:52 +0200

    Refactor district model interface

--- utils-java/src/main/java/it/unibo/drescue/model/District.java
+++ utils-java/src/main/java/it/unibo/drescue/model/District.java
@@ -12,12 +12,33 @@ public interface District extends ObjectModel {
     String getDistrictID();
 
     /**
+     * Sets the district ID
+     *
+     * @param districtID the district ID
+     */
+    void setDistrictID(String districtID);
+
+    /**
      * @return the district long name
      */
     String getDistrictLongName();
 
     /**
+     * Sets the district long name
+     *
+     * @param districtLongName the district long name
+     */
+    void setDistrictLongName(String districtLongName);
+
+    /**
      * @return the district population
      */
     int getPopulation();
+
+    /**
+     * Sets the district population
+     *
+     * @param population the district population
+     */
+    void setPopulation(int population);
 }
