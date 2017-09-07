3ed989a9e8044e5e6f8a7f8ef0bf095767722a5d
Sofia Rosetti <sofia.rosetti@studio.unibo.it>
2017-09-07 13:58:10 +0200

    Refactor rescue team builder

--- utils-java/src/main/java/it/unibo/drescue/model/RescueTeamImplBuilder.java
+++ utils-java/src/main/java/it/unibo/drescue/model/RescueTeamImplBuilder.java
@@ -2,44 +2,43 @@ package it.unibo.drescue.model;
 
 public class RescueTeamImplBuilder {
 
-    private String rescueTeamID = "";
-    private String password = "";
-    private String name = "";
-    private double latitude = 0;
-    private double longitude = 0;
-    private String phoneNumber = "";
+    private final RescueTeamImpl team;
+
+    public RescueTeamImplBuilder() {
+        this.team = new RescueTeamImpl();
+    }
 
     public RescueTeamImplBuilder setRescueTeamID(final String rescueTeamID) {
-        this.rescueTeamID = rescueTeamID;
+        this.team.setRescueTeamID(rescueTeamID);
         return this;
     }
 
     public RescueTeamImplBuilder setPassword(final String password) {
-        this.password = password;
+        this.team.setPassword(password);
         return this;
     }
 
     public RescueTeamImplBuilder setName(final String name) {
-        this.name = name;
+        this.team.setName(name);
         return this;
     }
 
     public RescueTeamImplBuilder setLatitude(final double latitude) {
-        this.latitude = latitude;
+        this.team.setLatitude(latitude);
         return this;
     }
 
     public RescueTeamImplBuilder setLongitude(final double longitude) {
-        this.longitude = longitude;
+        this.team.setLongitude(longitude);
         return this;
     }
 
     public RescueTeamImplBuilder setPhoneNumber(final String phoneNumber) {
-        this.phoneNumber = phoneNumber;
+        this.team.setPhoneNumber(phoneNumber);
         return this;
     }
 
     public RescueTeamImpl createRescueTeamImpl() {
-        return new RescueTeamImpl(this.rescueTeamID, this.password, this.name, this.latitude, this.longitude, this.phoneNumber);
+        return this.team;
     }
 }
