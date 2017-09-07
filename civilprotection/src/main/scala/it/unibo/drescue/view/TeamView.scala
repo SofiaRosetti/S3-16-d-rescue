f7c9d8ef5b04f7b24cf13acdd51d55e950cf6361
Sofia Rosetti <sofia.rosetti@studio.unibo.it>
2017-09-07 08:20:59 +0200

    Set resizable false to CP team view

--- civilprotection/src/main/scala/it/unibo/drescue/view/TeamView.scala
+++ civilprotection/src/main/scala/it/unibo/drescue/view/TeamView.scala
@@ -15,6 +15,7 @@ object TeamView extends JFXApp {
     title = "D-rescue"
     scene = new Scene {
       val grid = new GridPane {
+        resizable = false
         hgap = 10
         vgap = 10
         padding = Insets(100)
