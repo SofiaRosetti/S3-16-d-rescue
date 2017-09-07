c399ff4452666a401697f7c38c1260e6e95dd7af
Sofia Rosetti <sofia.rosetti@studio.unibo.it>
2017-09-07 10:54:17 +0200

    Refactor user model builder

--- utils-java/src/main/java/it/unibo/drescue/model/UserImplBuilder.java
+++ utils-java/src/main/java/it/unibo/drescue/model/UserImplBuilder.java
@@ -2,44 +2,49 @@ package it.unibo.drescue.model;
 
 public class UserImplBuilder {
 
-    private int userID;
+    /*private int userID;
     private String name;
     private String surname;
     private String email;
     private String password;
-    private String phoneNumber;
+    private String phoneNumber;*/
+    private final UserImpl user;
+
+    public UserImplBuilder() {
+        this.user = new UserImpl();
+    }
 
     public UserImplBuilder setUserID(final int userID) {
-        this.userID = userID;
+        this.user.setUserID(userID);
         return this;
     }
 
     public UserImplBuilder setName(final String name) {
-        this.name = name;
+        this.user.setName(name);
         return this;
     }
 
     public UserImplBuilder setSurname(final String surname) {
-        this.surname = surname;
+        this.user.setSurname(surname);
         return this;
     }
 
     public UserImplBuilder setEmail(final String email) {
-        this.email = email;
+        this.user.setEmail(email);
         return this;
     }
 
     public UserImplBuilder setPassword(final String password) {
-        this.password = password;
+        this.user.setPassword(password);
         return this;
     }
 
     public UserImplBuilder setPhoneNumber(final String phoneNumber) {
-        this.phoneNumber = phoneNumber;
+        this.user.setPhoneNumber(phoneNumber);
         return this;
     }
 
     public UserImpl createUserImpl() {
-        return new UserImpl(this.userID, this.name, this.surname, this.email, this.password, this.phoneNumber);
+        return this.user;
     }
 }
