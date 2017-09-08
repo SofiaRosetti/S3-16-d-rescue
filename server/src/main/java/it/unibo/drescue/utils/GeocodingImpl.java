9980e1200f8159806aa6c0b2026fbe0647817bcc
Sofia Rosetti <sofia.rosetti@studio.unibo.it>
2017-09-08 22:35:02 +0200

    Geocoding refactor

--- server/src/main/java/it/unibo/drescue/utils/GeocodingImpl.java
+++ server/src/main/java/it/unibo/drescue/utils/GeocodingImpl.java
@@ -9,15 +9,19 @@ import com.google.maps.model.LatLng;
 
 import java.io.IOException;
 
-public class Geocoding {
+public class GeocodingImpl implements Geocoding {
 
     private static final String KEY = "AIzaSyAhBFJafCe2FWr0ZejtEcQqvuB3TtyLZrE";
+    private static final String AREA = "ADMINISTRATIVE_AREA_LEVEL_2";
+    private static final String TYPES = "types";
+    private static final String SHORT_NAME = "shortName";
     private String response;
     private String district = "";
 
-    public Geocoding() {
+    public GeocodingImpl() {
     }
 
+    @Override
     public String getDistrict(final double latitude, final double longitude) {
 
         this.reverseGeocode(latitude, longitude);
@@ -27,15 +31,16 @@ public class Geocoding {
         final JsonArray array = jsonTree.getAsJsonArray();
         for (int i = 0; i < array.size(); i++) {
             final JsonObject object = (JsonObject) array.get(i);
-            final Object type = object.get("types");
+            final Object type = object.get(TYPES);
             final String typeString = type.toString();
-            if (typeString.contains("ADMINISTRATIVE_AREA_LEVEL_2")) {
-                this.district = object.get("shortName").toString().substring(1, 3);
+            if (typeString.contains(AREA)) {
+                this.district = object.get(SHORT_NAME).toString().substring(1, 3);
             }
         }
         return this.district;
     }
 
+    @Override
     public JsonObject getLatLng(final String address) {
 
         this.geocode(address);
@@ -47,7 +52,7 @@ public class Geocoding {
 
     private void reverseGeocode(final double latitude, final double longitude) {
         final GeoApiContext context = new GeoApiContext.Builder()
-                .apiKey(this.KEY)
+                .apiKey(KEY)
                 .build();
         final LatLng latlng = new LatLng(latitude, longitude);
         final GeocodingResult[] results;
@@ -55,7 +60,7 @@ public class Geocoding {
             results = GeocodingApi.reverseGeocode(context, latlng).await();
             final Gson gson = new GsonBuilder().setPrettyPrinting().create();
             this.response = gson.toJson(results[0].addressComponents);
-        } catch (final ApiException e) {
+        } catch (final ApiException e) {    // TODO handle exceptions
             e.printStackTrace();
         } catch (final InterruptedException e) {
             e.printStackTrace();
@@ -66,14 +71,14 @@ public class Geocoding {
 
     private void geocode(final String address) {
         final GeoApiContext context = new GeoApiContext.Builder()
-                .apiKey(this.KEY)
+                .apiKey(KEY)
                 .build();
         final GeocodingResult[] results;
         try {
             results = GeocodingApi.geocode(context, address).await();
             final Gson gson = new GsonBuilder().setPrettyPrinting().create();
             this.response = gson.toJson(results[0].geometry.location);
-        } catch (final ApiException e) {
+        } catch (final ApiException e) {    // TODO handle exceptions
             e.printStackTrace();
         } catch (final InterruptedException e) {
             e.printStackTrace();
