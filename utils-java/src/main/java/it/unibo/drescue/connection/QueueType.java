49bdfaa0f16890d69805bfba9933c44e24451a61
Elisa Casadio <elisa.casadio7@studio.unibo.it>
2017-09-08 17:56:20 +0200

    Add enum for queue type for communication

---
+++ utils-java/src/main/java/it/unibo/drescue/connection/QueueType.java
@@ -0,0 +1,29 @@
+package it.unibo.drescue.connection;
+
+/**
+ * Enum of types of queue which are useful for connection.
+ */
+public enum QueueType {
+    ALERTS_QUEUE_RPC("alerts_channel_RPC"),
+    AUTHENTICATION_QUEUE_RPC("authentication_channel_RPC"),
+    NEW_ALERT_QUEUE_PS("new_alert_channel_PS"),
+    PROFILE_QUEUE_RPC("profile_channel_RPC"),;
+
+    private final String queueName;
+
+    /**
+     * Creates a queue well formed.
+     *
+     * @param queueName the queue name
+     */
+    QueueType(final String queueName) {
+        this.queueName = queueName;
+    }
+
+    /**
+     * @return the queue name
+     */
+    public String getQueueName() {
+        return this.queueName;
+    }
+}
