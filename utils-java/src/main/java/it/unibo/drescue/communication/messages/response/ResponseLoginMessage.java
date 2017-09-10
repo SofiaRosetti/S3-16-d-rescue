3a4b5f895d475f45447f4d8f996df139f8f45efd
Elisa Casadio <elisa.casadio7@studio.unibo.it>
2017-09-10 11:26:58 +0200

    Change response message to login request by adding a list of events type

--- utils-java/src/main/java/it/unibo/drescue/communication/messages/response/ResponseLoginMessage.java
+++ utils-java/src/main/java/it/unibo/drescue/communication/messages/response/ResponseLoginMessage.java
@@ -1,10 +1,13 @@
 package it.unibo.drescue.communication.messages.response;
 
 import it.unibo.drescue.communication.builder.MessageBuilder;
+import it.unibo.drescue.model.EventType;
 import it.unibo.drescue.model.User;
 
+import java.util.List;
+
 /**
- * Interface modelling a response message to the login request containing user data.
+ * Interface modelling a response message to the login request containing user data and a list of events type.
  */
 public interface ResponseLoginMessage extends MessageBuilder {
 
@@ -12,4 +15,10 @@ public interface ResponseLoginMessage extends MessageBuilder {
      * @return user data
      */
     User getUser();
+
+    /**
+     * @return list of events type
+     */
+    List<EventType> getEventsType();
+
 }
