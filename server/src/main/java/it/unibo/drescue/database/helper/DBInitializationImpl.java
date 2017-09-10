69715d88f02a38a925f34c65442a0fd06974930f
Samantha Bandini <samantha.bandini3@studio.unibo.it>
2017-09-10 12:30:11 +0200

    Add civil protection and cp area automic insert for db from json files

--- server/src/main/java/it/unibo/drescue/database/helper/DBInitializationImpl.java
+++ server/src/main/java/it/unibo/drescue/database/helper/DBInitializationImpl.java
@@ -2,12 +2,11 @@ package it.unibo.drescue.database.helper;
 
 import it.unibo.drescue.database.DBConnection;
 import it.unibo.drescue.database.JsonFileUtils;
+import it.unibo.drescue.database.dao.CivilProtectionDao;
+import it.unibo.drescue.database.dao.CpAreaDao;
 import it.unibo.drescue.database.dao.DistrictDao;
 import it.unibo.drescue.database.dao.EventTypeDao;
-import it.unibo.drescue.model.District;
-import it.unibo.drescue.model.DistrictImpl;
-import it.unibo.drescue.model.EventType;
-import it.unibo.drescue.model.EventTypeImpl;
+import it.unibo.drescue.model.*;
 
 import java.sql.SQLException;
 import java.util.List;
@@ -73,5 +72,59 @@ public class DBInitializationImpl implements DBInitialization {
             //TODO Handle Exception
         }
 
+    }
+
+    @Override
+    public void insertAllCivilProtectionsFrom(final String pathFile) {
+
+        CivilProtectionDao civilProtectionDao = null;
+        //Getting districts from file
+        final List<CivilProtectionImpl> civilProtectionList =
+                this.jsonFileUtils.getListFromJsonFile(pathFile, CivilProtectionImpl[].class);
+        if (civilProtectionList == null) {
+            //TODO Exception
+            System.out.println("cp: null");
+            return;
+        }
+        //Getting Dao
+        try {
+            civilProtectionDao = (CivilProtectionDao) this.dbConnection.getDAO(DBConnection.Table.CIVIL_PROTECTION);
+        } catch (final SQLException e) {
+            //TODO Exception
+            e.printStackTrace();
+            return;
+        }
+        //Inserting districts
+        for (final CivilProtection civilProtection : civilProtectionList) {
+            civilProtectionDao.insert(civilProtection);
+            //TODO handle exception
+        }
+    }
+
+    @Override
+    public void insertAllCpAreasFrom(final String pathFile) {
+
+        CpAreaDao cpAreaDao = null;
+        //Getting districts from file
+        final List<CpAreaImpl> cpAreas =
+                this.jsonFileUtils.getListFromJsonFile(pathFile, CpAreaImpl[].class);
+        if (cpAreas == null) {
+            //TODO Exception
+            System.out.println("cpareas: null");
+            return;
+        }
+        //Getting Dao
+        try {
+            cpAreaDao = (CpAreaDao) this.dbConnection.getDAO(DBConnection.Table.CP_AREA);
+        } catch (final SQLException e) {
+            //TODO Exception
+            e.printStackTrace();
+            return;
+        }
+        //Inserting districts
+        for (final CpArea cpArea : cpAreas) {
+            cpAreaDao.insert(cpArea);
+            //TODO handle exception
+        }
     }
 }
--- server/src/main/java/it/unibo/drescue/database/helper/DBInitializationStart.java
+++ server/src/main/java/it/unibo/drescue/database/helper/DBInitializationStart.java
@@ -8,6 +8,8 @@ public class DBInitializationStart {
 
     private static final String DISTRICTS_FILE = "/districts.json";
     private static final String EVENT_TYPES_FILE = "/event_types.json";
+    private static final String CIVIL_PROTECTION_FILE = "/civil_protections.json";
+    private static final String CP_AREAS_FILE = "/cp_areas.json";
 
     /**
      * Used to initialize DB with static tables contents
@@ -24,6 +26,8 @@ public class DBInitializationStart {
 
         dbInitialization.insertAllEventTypesFrom(FILE_PATH + EVENT_TYPES_FILE);
 
-        //TODO insert all Civil_Protections and CP_areas
+        dbInitialization.insertAllCivilProtectionsFrom(FILE_PATH + CIVIL_PROTECTION_FILE);
+
+        dbInitialization.insertAllCpAreasFrom(FILE_PATH + CP_AREAS_FILE);
     }
 }
