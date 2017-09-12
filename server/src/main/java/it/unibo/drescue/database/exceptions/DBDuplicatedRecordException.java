fc7f0bb7d92b867858a022eb0a7e55154ad173eb
Samantha Bandini <samantha.bandini3@studio.unibo.it>
2017-09-12 12:27:11 +0200

    Add other messages and getters to exceptions

--- server/src/main/java/it/unibo/drescue/database/exceptions/DBDuplicatedRecordException.java
+++ server/src/main/java/it/unibo/drescue/database/exceptions/DBDuplicatedRecordException.java
@@ -1,4 +1,19 @@
 package it.unibo.drescue.database.exceptions;
 
 public class DBDuplicatedRecordException extends Exception {
+
+    private static final String DUPLICATED_MESSAGE =
+            "Exception for trying to insert a duplicated record";
+    
+    private final String message;
+
+    public DBDuplicatedRecordException() {
+        this.message = DUPLICATED_MESSAGE;
+    }
+
+    @Override
+    public String getMessage() {
+        return this.message;
+    }
+
 }
