cf06b102e892c2f5b97a3aeae614442f466b765f
Sofia Rosetti <sofia.rosetti@studio.unibo.it>
2017-09-13 15:07:03 +0200

    Create object for CP view constants

---
+++ civilprotection/src/main/scala/it/unibo/drescue/view/ViewConstants.scala
@@ -0,0 +1,34 @@
+package it.unibo.drescue.view
+
+object ViewConstants {
+
+  val gap = 10
+
+  val insets5 = 5
+  val insets10 = 10
+  val insets20 = 20
+  val insets30 = 30
+  val insets50 = 50
+  val insets100 = 100
+
+  val font18 = 18
+  val font20 = 20
+  val font25 = 25
+  val font30 = 30
+
+  val columnRow0 = 0
+  val columnRow1 = 1
+  val columnRow2 = 2
+  val columnRow3 = 3
+  val columnRow4 = 4
+  val columnRow5 = 5
+  val columnRow6 = 6
+
+  val widthHeight60 = 60
+  val widthHeight100 = 100
+  val widthHeight150 = 150
+  val widthHeight200 = 200
+  val widthHeight250 = 250
+  val widthHeight300 = 300
+  val widthHeight400 = 400
+}
