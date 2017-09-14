f8c80d6419abd01f48457079ad01f696fe2b049c
agleoni <annagiulia.leoni@studio.unibo.it>
2017-09-14 12:41:15 +0200

    Add new type of message to request alerts from cp and its tests

--- server/src/test/scala/it/unibo/drescue/communication/messages/ForwardMessageTest.scala
+++ server/src/test/scala/it/unibo/drescue/communication/messages/ForwardMessageTest.scala
@@ -6,23 +6,23 @@ import org.scalatest.FunSuite
 import scala.collection.mutable.ListBuffer
 
 object ForwardMessageTest {
-  private val cpID1 = "RA01"
-  private val cpID2 = "RA02"
+  private val CpID1 = "RA01"
+  private val CpID2 = "RA02"
 }
 
 class ForwardMessageTest extends FunSuite {
 
   val alertImpl: AlertImpl = new AlertImplBuilder().createAlertImpl()
-  val cpIDList: ListBuffer[String] = ListBuffer(ForwardMessageTest.cpID1, ForwardMessageTest.cpID2)
+  val cpIDList: ListBuffer[String] = ListBuffer(ForwardMessageTest.CpID1, ForwardMessageTest.CpID2)
   val forwardMessage = ForwardObjectMessage(cpIDList, alertImpl)
 
   test("Check MessageType") {
-    assert(forwardMessage.getMessageType === MessageType.FORWARD_MESSAGE.getMessageType)
+    assert(forwardMessage.getMessageType == MessageType.FORWARD_MESSAGE.getMessageType)
   }
 
   test("Check ForwardMessage fields") {
-    assert(cpIDList === forwardMessage.cpIDList)
-    assert(forwardMessage.objectModel.asInstanceOf[AlertImpl].getClass === classOf[AlertImpl])
+    assert(cpIDList == forwardMessage.cpIDList)
+    assert(forwardMessage.objectModel.asInstanceOf[AlertImpl].getClass == classOf[AlertImpl])
   }
 
 
