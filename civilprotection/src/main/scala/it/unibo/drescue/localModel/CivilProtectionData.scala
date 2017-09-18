ee121cab18de67d479e79ed8040e2ce2d92c9768
Samantha Bandini <samantha.bandini3@studio.unibo.it>
2017-09-18 15:06:38 +0200

    Create getter and setter in model with notification for observer

--- civilprotection/src/main/scala/it/unibo/drescue/localModel/CivilProtectionData.scala
+++ civilprotection/src/main/scala/it/unibo/drescue/localModel/CivilProtectionData.scala
@@ -1,19 +1,105 @@
 package it.unibo.drescue.localModel
 
+import java.util
 import java.util.{ArrayList, List}
 
 import it.unibo.drescue.model.RescueTeam
 
 case class CivilProtectionData() extends Observable {
 
-  var cpID: String = null
+  private var _cpID: String = null
 
-  var lastAlerts: List[AlertEntry] = new ArrayList[AlertEntry]()
+  private var _lastAlerts: List[AlertEntry] = new ArrayList[AlertEntry]()
 
-  var enrolledRescueTeams: List[RescueTeam] = new ArrayList[RescueTeam]()
+  private var _enrolledRescueTeams: List[RescueTeam] = new ArrayList[RescueTeam]()
 
-  var notEnrolledRescueTeams: List[RescueTeam] = new ArrayList[RescueTeam]()
+  private var _notEnrolledRescueTeams: List[RescueTeam] = new ArrayList[RescueTeam]()
 
-  var enrolledTeamInfoList: List[EnrolledTeamInfo] = new ArrayList[EnrolledTeamInfo]()
+  private var _enrolledTeamInfoList: List[EnrolledTeamInfo] = new ArrayList[EnrolledTeamInfo]()
+
+  /**
+    * TODO
+    *
+    * @return
+    */
+  def cpID = _cpID
+
+  /**
+    * TODO
+    *
+    * @param value
+    */
+  def cpID_=(value: String): Unit = {
+    _cpID = value
+  }
+
+  //TODO
+  /**
+    * TODO
+    *
+    * @return
+    */
+  def lastAlerts = _lastAlerts
+
+  /**
+    * TODO
+    *
+    * @param list
+    */
+  def lastAlerts_=(list: util.List[AlertEntry]): Unit = {
+    _lastAlerts = list
+    notifyObserver(Observers.Home)
+  }
+
+  /**
+    * TODO
+    *
+    * @return
+    */
+  def enrolledRescueTeams = _enrolledRescueTeams
+
+  /**
+    * TODO
+    *
+    * @param list
+    */
+  def enrolledRescueTeams_=(list: util.List[RescueTeam]): Unit = {
+    _enrolledRescueTeams = list
+    notifyObserver(Observers.ManageRescue)
+  }
+
+  /**
+    * TODO
+    *
+    * @return
+    */
+  def notEnrolledRescueTeams = _notEnrolledRescueTeams
+
+  /**
+    * TODO
+    *
+    * @param list
+    */
+  def notEnrolledRescueTeams_=(list: util.List[RescueTeam]): Unit = {
+    _notEnrolledRescueTeams = list
+    notifyObserver(Observers.EnrollTeam)
+  }
+
+  /**
+    * TODO
+    *
+    * @return
+    */
+  def enrolledTeamInfoList = _enrolledTeamInfoList
+
+  /**
+    * TODO
+    *
+    * @param list
+    */
+  def enrolledTeamInfoList_=(list: util.List[EnrolledTeamInfo]): Unit = {
+    _enrolledTeamInfoList = list
+    notifyObserver(Observers.ManageRescue)
+  }
 
 }
