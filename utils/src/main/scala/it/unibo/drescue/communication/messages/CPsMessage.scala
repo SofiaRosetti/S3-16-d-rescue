25347b712d364e68966019d5a5381e3fe3486371
agleoni <annagiulia.leoni@studio.unibo.it>
2017-09-20 16:42:15 +0200

    Add messages to get all CPs of a specific rescue team

---
+++ utils/src/main/scala/it/unibo/drescue/communication/messages/CPsMessage.scala
@@ -0,0 +1,23 @@
+import it.unibo.drescue.communication.builder.MessageBuilder
+import it.unibo.drescue.communication.messages.{AbstractMessage, Message, MessageType}
+import it.unibo.drescue.model.CivilProtectionImpl
+
+/**
+  * Trait modelling a message containing all CPs
+  * related to the rescue team selected to start or
+  * stop rescue.
+  */
+sealed trait CPsMessage {
+
+  /**
+    *
+    * @return the list of CPs related to the rescue team
+    */
+  def cpList: java.util.List[CivilProtectionImpl]
+}
+
+case class CPsMessageImpl(override val cpList: java.util.List[CivilProtectionImpl])
+  extends AbstractMessage(MessageType.CPS_MESSAGE) with CPsMessage with MessageBuilder {
+
+  override def build(): Message = this
+}
