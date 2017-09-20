93ad8ed3488940b55ff24c978351331ea9fe8dd0
agleoni <annagiulia.leoni@studio.unibo.it>
2017-09-20 15:30:59 +0200

    Add observable buffer to ManageRescuesGrid TableView

--- civilprotection/src/main/scala/it/unibo/drescue/controller/ManageRescuesControllerImpl.scala
+++ civilprotection/src/main/scala/it/unibo/drescue/controller/ManageRescuesControllerImpl.scala
@@ -1,26 +1,37 @@
 package it.unibo.drescue.controller
 
 import it.unibo.drescue.connection.RabbitMQImpl
-import it.unibo.drescue.localModel.Observers
+import it.unibo.drescue.localModel.{EnrolledTeamInfo, Observers}
+
+import scalafx.collections.ObservableBuffer
 
 class ManageRescuesControllerImpl(private var mainController: MainControllerImpl,
                                   var rabbitMQ: RabbitMQImpl) extends Observer {
 
   mainController.model.addObserver(Observers.ManageRescue, this)
 
-  /**
-    * TODO
-    */
+  var obsBuffer = new ObservableBuffer[EnrolledTeamInfo]()
+
   override def onReceivingNotification(): Unit = {
-    //TODO
+    obsBuffer.clear()
+    mainController.model.enrolledTeamInfoList.forEach(
+      (enrolledTeamInfo: EnrolledTeamInfo) => {
+        obsBuffer add enrolledTeamInfo
+      }
+    )
   }
 
   def sendPressed(wantedRescueTeamID: String) = {
 
+    //TODO get commonCP (see messages in ServerSideHandler and create Server Service)
+
+    //TODO start Ricart Agrawala's Algorithm to occupy the given rescueTeamID
 
   }
 
-  def stopPressed(wantedRescueTeamID: String) = {}
+  def stopPressed(wantedRescueTeamID: String) = {
+
+  }
 
   def backPress() = {
     mainController.changeView("Home")
